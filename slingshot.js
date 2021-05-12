1e71248d30269e0be88ddb305043a9c60449fc01
vedant017 <76477542+vedant017@users.noreply.github.com>
2021-05-12 18:51:52 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,42 @@
+class Slingshot{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.08,
+            length:10
+        }
+        this.sling=Constraint.create(options)
+        this.pointB=pointB
+        World.add(world,this.sling)
+        this.sling1=loadImage("sprites/sling1.png")
+        this.sling2=loadImage("sprites/sling2.png")
+        this.sling3=loadImage("sprites/sling3.png")
+    }
+    fly(){
+        this.sling.bodyA=null
+    }
+    attatch(body){
+        this.sling.bodyA=body
+    }
+    display(){
+        image(this.sling1,200,20)
+        image(this.sling2,170,20)
+        push()
+        strokeWeight(4)
+        stroke("#50250E")
+        if(this.sling.bodyA){
+            if(this.sling.bodyA.position.x<220){
+
+        line(this.sling.bodyA.position.x-20,this.sling.bodyA.position.y,this.pointB.x-10,this.pointB.y)
+        line(this.sling.bodyA.position.x-20,this.sling.bodyA.position.y,this.pointB.x+30,this.pointB.y)
+        image(this.sling3,this.sling.bodyA.position.x-30,this.sling.bodyA.position.y-10,15,30)
+            }else{
+                line(this.sling.bodyA.position.x+25,this.sling.bodyA.position.y,this.pointB.x-10,this.pointB.y)
+        line(this.sling.bodyA.position.x+25,this.sling.bodyA.position.y,this.pointB.x+30,this.pointB.y)
+        image(this.sling3,this.sling.bodyA.position.x+25,this.sling.bodyA.position.y-10,15,30)
+            }
+        }
+        pop()
+    }
+}
